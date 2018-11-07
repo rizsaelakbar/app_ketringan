f10eea09295980752d25b5309b969bc6740907b5
Rizsa El Akbar <rizsalekbar99@gmail.com>
2018-11-07 13:34:22 +0700

    versi 1

---
+++ app/src/main/java/com/example/rizsaelakbar/app_ketringan/V_opening.java
@@ -0,0 +1,13 @@
+package com.example.rizsaelakbar.app_ketringan;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class V_opening extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_v_opening);
+    }
+}
